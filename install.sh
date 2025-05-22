6807d2b8329e844b99f74bbf3e15de9fb10f16dc
Maks <154363174+maksdl@users.noreply.github.com>
2025-05-22 20:10:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ mkdir -p ~/holesky-node/{execution,consensus}/data && cd ~/holesky-node
 
 # Создаем docker-compose.yml
 cat << 'EOF' > docker-compose.yml
+version: '3.8'
+
 services:
   execution:
     image: ethereum/client-go:stable
@@ -64,6 +66,7 @@ services:
 networks:
   holesky-net:
     driver: bridge
+
 EOF
 
 # Генерируем JWT-секрет
